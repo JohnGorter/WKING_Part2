b397d104c37b4366ffd7bf8509c7de1be3c34eaa
JohnGorter <john.gorter@gmail.com>
2014-07-08 16:51:45 +0200

    bootstrap

--- scripts/mainjq.js
+++ scripts/mainjq.js
@@ -1,12 +1,48 @@
 
+  function validate(inp, level){
+    var input = $(inp);
+    var parent = input.parent();
+    if (level == 2) parent = parent.parent();
+        
+    if (input.val() == "") {
+         input.focus();
+         parent.addClass("has-error");
+         parent.append("<span class='glyphicon glyphicon-remove form-control-feedback'></span>");
+         return true;
+    } else { 
+        parent.removeClass("has-error");
+        $(".glyphicon", parent).remove();
+
+    }
+    return false;
+  }
+
    $(function(){
     
         $("#btnSave").click(function() {
-            $("<li>").append($("<span>").addClass("visible").text($("#title").val())).append($("<span>").text($("#description").val())).append($("<span>").text($("#score").val())).append($("<span>").text($("#date").val())).appendTo($("#ullist"));            
+            
+            if (validate("#team1", 2)) return;
+            if (validate("#team2", 2)) return;
+            if (validate("#description", 1)) return;
+            if (validate("#date", 1)) return;
+            if (validate("#scoreteam1", 2)) return;
+            if (validate("#scoreteam2", 2)) return;
+           
+             
+             $("#description").parent().removeClass("has-error");
+             $("#date").parent().removeClass("has-error");
+             $("#scoreteam1").parent().parent().removeClass("has-error");
+             
+            $("<li>").append($("<span>")
+                             .addClass("visible").text($("#team1").val() + "-" + $("#team2").val()))
+                             .append($("<span>").text($("#description").val()))
+                             .append($("<span>").text($("#scoreteam1").val() + "-" + $("#scoreteam2").val()))
+                             .append($("<span>").text($("#date").val()))
+                    .appendTo($("#ullist"));            
         });
         
         $("#ullist").click(function(){
-            $("#details_title").html($(window.event.target).text() + " <small>" + $(window.event.target).next().next().text() + "</small>");
+            $("#details_title").html($(window.event.target).text() + " <small> [score " + $(window.event.target).next().next().text() + "]</small>");
             $("#details_description").text($(window.event.target).next().text());
          });
         
